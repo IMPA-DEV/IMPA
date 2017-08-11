0969a2abdaae9ff5f08f511860c515955bf23466
Alexandr Oles <sane490@gmail.com>
2017-08-11 10:53:30 +0300

    Актуальная версия 11.08.17

--- js/common.js
+++ js/common.js
@@ -9,7 +9,8 @@ $(function() {
 			url: "mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
-			alert("Thank you!");
+            $( "#popup-form" ).slideUp( "slow", function() { });
+            $( "#popup-thanks" ).slideDown( "slow", function() { });
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");
@@ -33,19 +34,29 @@ $(function() {
 
     $('.close-button').click(function(){
         //$('#popup-form,#popup-thanks').hide();
-        $( "#popup-form,#popup-thanks,#popup-sucdetails" ).slideUp( "slow", function() { });
+        //$( "#popup-form,#popup-thanks,#popup-sucdetails" ).slideUp( "slow", function() { });
+        $('#popup-form .popup,#popup-thanks .popup,#popup-sucdetails .popup').animateCss('zoomOut');
+        $('#popup-form,#popup-thanks,#popup-sucdetails').fadeOut("500");
+        setTimeout(function(){
+            $('#popup-form .popup,#popup-thanks .popup,#popup-sucdetails .popup').removeClass('zoomOut');
+        }, 500);
+
+
+
     });
 
     $('a[href="#free"]').click(function(){
         //jQuery("#popup-form").show();
-        $( "#popup-sucdetails" ).slideUp( "slow", function() { });
-        $( "#popup-form" ).slideDown( "slow", function() { });
+        $('#popup-sucdetails').hide();
+        $('#popup-form').fadeIn("500");
+        $('#popup-form .popup').animateCss('zoomIn');
 
 
     });
 
     $('a[href="#succ-details"]').click(function(){
-        $( "#popup-sucdetails" ).slideDown( "slow", function() { });
+        $('#popup-sucdetails').fadeIn("500");
+        $('#popup-sucdetails .popup').animateCss('zoomIn');
     });
 
     $('#navigation').click(function () {
@@ -69,5 +80,15 @@ $(function() {
             arrows:false,
             dots: true
         });
+    }
+});
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+        return this;
     }
 });
